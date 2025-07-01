92d00ba0df78d34a507219246de200bea4dc7cf2
Rishikesh Bharat Joshi <129749828+rishi1203@users.noreply.github.com>
2025-07-01 20:43:58 +0530

    Feat: Created CRUD operations using rest_framework

--- api/views.py
+++ api/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET','POST'])
 def studentsView(request):
-    student = Student.objects.all()
-    student_list = list(student.values())
-    return JsonResponse(student_list,safe = False)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])   
+def studentDetailView(request,pk):
+    try:
+        student = Student.objects.get(pk = pk)
+    except Student.DoesNotExist:
+        return Response(status= status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data,status= status.HTTP_200_OK )
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+    elif request.method =='DELETE':
+        student.delete()
+        return Response(status= status.HTTP_204_NO_CONTENT)
+    
+
+
