6ae000b803472f86a9eb5491605dd2d8a49b0c25
Rishikesh Bharat Joshi <129749828+rishi1203@users.noreply.github.com>
2025-07-02 18:02:23 +0530

    Feat:Created new Employees App

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('students',views.studentsView),
     path('students/<int:pk>/', views.studentDetailView),
+
+    path('employees',views.Employees.as_view())
 ]
